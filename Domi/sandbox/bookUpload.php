72cfe033e085f2ff211a0446a2c9fdcfbc3052bb
LiwiaMa <liwiamatuszczak@gmail.com>
2021-01-19 11:23:10 +0100

    ladda upp img

--- Domi/sandbox/bookUpload.php
+++ Domi/sandbox/bookUpload.php
@@ -14,81 +14,116 @@
 include "./conn.php";
 ?>
 
-        <?php
-        if (isset($_POST['title'], $_POST['author'], $_POST['genre'])) {
+<!DOCTYPE html>
+<html lang="en">
 
-            // Ta emot filen
-            $file = $_FILES['image'];
-            var_dump($file);
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Upload books</title>
+    <link rel="stylesheet" href="./style/collection-styles.css">
+    <link rel="stylesheet" href="./style/menu.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css%22%3E">
+</head>
 
-            $title = $_POST['title'];
-            $author = $_POST['author'];
-            $genre = $_POST['genre'];
-
-            //  Filens namn
-            $fileName = $file["name"];
-            $fileSize = $file["size"];
-            $fileType = $file["type"];
-            $fileTempName = $file["tmp_name"];
-            $error = $file["error"];
-
-            // Tillåtna filtyper
-            $allowed = ["jpeg", "png"];
-            
-
-            // Bildens filtyp
-            $delar = explode("/", $fileType);
-            $imageType = $delar[1];
-            var_dump($imageType);
-
-            // Är filen tillåten?
-            if (in_array($imageType, $allowed)) {
-
-                // Blev det något felmeddelande?
-                if ($error === 0) {
-                    
-                    // Är filen för stor? eller for liten, whatever
-                    if ($fileSize <= 3000000) {
-                        
-                        // Skapa ett unikt namn
-                        $fileNewName = uniqid("", true). "$imageType";
-
-                        // vart filen skall hamna
-                        $fileDestination = "bilder/$fileNewName";
-
-                        // Äntligen! Flytta filen in i mappen
-                        move_uploaded_file($fileTempName, $fileDestination);
-                        echo "<p>Filen är uppladdat </p>";
-                        var_dump($fileDestination);
-                    } 
-                    else {
-                        echo "<p>Filen är fööööööör stor!</p>";
-                    }
-                }else {
-                    echo "<p>Något blev fel</p>";
-                }
-                
+<body>
+    <header>
+        <div>
+            <a href="https://www.ntigymnasiet.se/stockholm/"><img id="nti" src="./bilder/nti_logo_svart.svg"
+                    alt="nti"></a>
+        </div>
+        <div class="topnav" id="myTopnav">
+            <a href="collection-page.php">Collection</a>
+            <a href="collection-page.php" class="active">Collection</a>
+            <a href="#">Sing out</a>
+            <a href="#">Homepage</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+    </header>
+    <main>
+        <div class="page-grid">
+            <div class="collection">
+                <h1>Upload new book</h1>
+                <form action="bookUpload.php" method="POST" enctype="multipart/form-data">
+                    <label>Title <input type="text" name="title"></label>
+                    <label>Author <input type="text" name="author"></input></label>
+                    <label>Genre <input type="text" name="genre"></input></label>
+                    <label for="image">Image</label>
+                    <input type="file" name="image">
+                    <button type="submit" name="submit">Save</button>
+                </form>
+            </div>
+        </div>
+    </main>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
             } else {
-                echo "<p>Sorry, du får bara ladda upp jpg, png eller gif!.</p>";
+                x.className = "topnav";
             }
-             // Sql satsen
-            var_dump($fileDestination);
-            $sql = "INSERT INTO collection (title, author, genre, image) VALUES ('$title', '$author', '$genre', '$fileTempName')";
-
-             // Steg 2: nu kör vi sql-saten
-             $result = $conn->query($sql);
-
-             // Gick det bra att köra sql-satsen
-             if (!$result) {
-                 die("There is something wrong with SQL-set" . $conn->error);
-             } else {
-                 echo "<p>The post has been succesfully registred</p>";
-             }
- 
-             // Steg 3: Stänga ned anslutningen
-             $conn->close();
-
-            
         }
-        ?>
-    
+    </script>
+
+<?php
+if (isset($_POST['submit'])) {
+
+    // Ta emot filen
+    $file = $_FILES["image"];
+    var_dump($file);
+    //  Filens namn
+    $fileName = $file["name"];
+    $fileSize = $file["size"];
+    $fileType = $file["type"];
+    $fileTempName = $file["tmp_name"];
+    $error = $file["error"];
+
+    $author = $_POST["author"];
+    $title = $_POST["title"];
+    // Tillåtna filtyper
+    $allowed = ["jpg", "png", "jpeg"];
+
+
+    // Bildens filtyp
+    $delar = explode("/", $fileType);
+    $imageType = $delar[1];
+
+    // Är filen tillåten?
+    if (in_array($imageType, $allowed)) {
+
+        // Blev det något felmeddelande?
+        if ($error === 0) {
+
+            // Är filen för stor? eller for liten, whatever
+            if ($fileSize <= 200000) {
+
+                // Skapa ett unikt namn
+                $fileNewName = uniqid("", true) . "$imageType";
+
+                // vart filen skall hamna
+                $fileDestination = "photo/$fileNewName";
+
+                $path = "photo/";
+                // Äntligen! Flytta filen in i mappen
+                move_uploaded_file($fileTempName, $fileDestination);
+                $sql = "INSERT INTO collection (title, author, image) VALUES ('$title', '$author', '$fileDestination')";
+                $conn->query($sql);
+
+                echo "<p>Filen är uppladdat </p>";
+                header("Location: book-upload.html");
+            } else {
+                echo "<p>Filen är fööööööör stor!</p>";
+            }
+        } else {
+            echo "<p>Något blev fel</p>";
+        }
+    } else {
+        echo "<p>Sorry, du får bara ladda upp jpg, png eller gif!.</p>";
+    }
+    $conn->close();
+}
+?>
