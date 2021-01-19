4f5c324bf58f4122f40ee1ee2fdd7e2a2e47a22b
LiwiaMa <liwiamatuszczak@gmail.com>
2021-01-19 11:50:28 +0100

    book upload

--- Domi/sandbox/bookUpload.php
+++ Domi/sandbox/bookUpload.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /**
@@ -13,22 +14,25 @@
 // Include är att klistrar in det. Ungefär som css
 include "./conn.php";
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Write books</title>
+    <title>Upload books</title>
     <link rel="stylesheet" href="./style/collection-styles.css">
     <link rel="stylesheet" href="./style/menu.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css%22%3E">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css%22%3E">
 </head>
 
 <body>
     <header>
         <div>
-            <a href="https://www.ntigymnasiet.se/stockholm/"><img id="nti" src="./bilder/nti_logo_svart.svg" alt="nti"></a>
+            <a href="https://www.ntigymnasiet.se/stockholm/"><img id="nti" src="./bilder/nti_logo_svart.svg"
+                    alt="nti"></a>
         </div>
         <div class="topnav" id="myTopnav">
             <a href="collection-page.php">Collection</a>
@@ -40,54 +44,90 @@ include "./conn.php";
             </a>
         </div>
     </header>
-    <div class="page-grid">
-        <div class="collection">
-            <h1>Write new books</h1>
-            <form action="#" method="POST">
-                <label>Title <input type="text" name="title"></label>
-                <label>Author <input type="text" name="author"></input></label>
-                <label for="image">Image</label>
-                <input type="file" name="image">
-                <button>Save</button>
-            </form>
+    <main>
+        <div class="page-grid">
+            <div class="collection">
+                <h1>Upload new book</h1>
+                <form action="bookUpload.php" method="POST" enctype="multipart/form-data">
+                    <label>Title <input type="text" name="title"></label>
+                    <label>Author <input type="text" name="author"></input></label>
+                    <label>Genre <input type="text" name="genre"></input></label>
+                    <label for="image">Image</label>
+                    <input type="file" name="image">
+                    <button type="submit" name="submit">Save</button>
+                </form>
+            </div>
         </div>
-        <?php
-        $target = "./book-images";
-        $target = $target . basename($_FILES['image']);
-
-        // Ta emot det som skickas
-        $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
-        $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_STRING);
-        $pic = ($_FILES['image']);
-
-
-        // Om data finns...  //trejde variabel//
-        if ($title && $author) {
-            // mysql -> insert -> runrik och text -> copy php code
-            // Sql satsen
-            $sql = "INSERT INTO collection (title, author, image) VALUES ('$title', '$author', '$pic')";
-
-            // Var filen skall hamna
-            $target = "./book-images/$pic";
-
-            // Äntligen! Flytta filen in i mappen
-            move_uploaded_file($pic, $target);
-
-
-            // Steg 2: nu kör vi sql-saten
-            $result = $conn->query($sql);
-
-            // Gick det bra att köra sql-satsen
-            if (!$result) {
-                die("There is something wrong with SQL-set" . $conn->error);
+    </main>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
             } else {
-                echo "<p>The post has been succesfully registred</p>";
+                x.className = "topnav";
             }
-
-            // Steg 3: Stänga ned anslutningen
-            $conn->close();
         }
-        ?>
+    </script>
+
+<?php
+if (isset($_POST['submit'])) {
+
+    // Ta emot filen
+    $file = $_FILES["image"];
+    var_dump($file);
+    //  Filens namn
+    $fileName = $file["name"];
+    $fileSize = $file["size"];
+    $fileType = $file["type"];
+    $fileTempName = $file["tmp_name"];
+    $error = $file["error"];
+
+    $author = $_POST["author"];
+    $title = $_POST["title"];
+    // Tillåtna filtyper
+    $allowed = ["jpg", "png", "jpeg"];
+
+
+    // Bildens filtyp
+    $delar = explode("/", $fileType);
+    $imageType = $delar[1];
+
+    // Är filen tillåten?
+    if (in_array($imageType, $allowed)) {
+
+        // Blev det något felmeddelande?
+        if ($error === 0) {
+
+            // Är filen för stor? eller for liten, whatever
+            if ($fileSize <= 200000) {
+
+                // Skapa ett unikt namn
+                $fileNewName = uniqid("", true) . "$imageType";
+
+                // vart filen skall hamna
+                $fileDestination = "photo/$fileNewName";
+
+                $path = "photo/";
+                // Äntligen! Flytta filen in i mappen
+                move_uploaded_file($fileTempName, $fileDestination);
+                $sql = "INSERT INTO collection (title, author, image) VALUES ('$title', '$author', '$fileDestination')";
+                $conn->query($sql);
+
+                echo "<p>Filen är uppladdat </p>";
+                header("Location: book-upload.html");
+            } else {
+                echo "<p>Filen är fööööööör stor!</p>";
+            }
+        } else {
+            echo "<p>Något blev fel</p>";
+        }
+    } else {
+        echo "<p>Sorry, du får bara ladda upp jpg, png eller gif!.</p>";
+    }
+    $conn->close();
+}
+?>
     </div>
 </body>
 
--- Domi/sandbox/collection-page.php
+++ Domi/sandbox/collection-page.php
@@ -4,7 +4,6 @@ include "./conn.php";
 ?>
 <!DOCTYPE html>
 <html lang="sv">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,21 +12,21 @@ include "./conn.php";
     <link rel="stylesheet" href="./style/menu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 <body>
     <header>
-        <div>
-            <a href="https://www.ntigymnasiet.se/stockholm/"><img id="nti" src="./bilder/nti_logo_svart.svg" alt="nti"></a>
-        </div>
-        <div class="topnav" id="myTopnav">
-            <a href="#" class="active">Collection</a>
-            <a href="bookUpload.php">Upload</a>
-            <a href="#">Collection</a>
-            <a href="#">Homepage</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </div>
+            <div>
+                <a href="https://www.ntigymnasiet.se/stockholm/"><img id="nti" src="./bilder/nti_logo_svart.svg"
+                        alt="nti"></a>
+            </div>
+            <div class="topnav" id="myTopnav">
+                <a href="#" class="active">Collection</a>
+                <a href="bookUpload.php">Upload</a>
+                <a href="#">Collection</a>
+                <a href="#">Homepage</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
     </header>
     <main>
         <div class="page-grid">
@@ -36,42 +35,40 @@ include "./conn.php";
                     <h2>Genres</h2>
                 </div>
                 <div class="genres-wrapper">
-                    <button class="genre">Thriller</button>
-                    <button class="genre">Horror</button>
-                    <button class="genre">Romance</button>
-                    <button class="genre">Romance</button>
-                    <button class="genre">Romance</button>
+                    <button class="genre" name="thriller">Thriller</button>
+                    <button class="genre" name="horror">Horror</button>
+                    <button class="genre" name="romance">Romance</button>
+                    <button class="genre" name="drama">Drama</button>
+                    <button class="genre" name="sciFi">Sci-fi</button>
                 </div>
             </div>
             <div class="collection">
                 <div class="title">
                     <h2>Collection</h2>
                 </div>
+                
+                    <?php
 
-                <?php
+                    echo "<div>";
+                    
+                    $sql = "SELECT * FROM collection";
+                    $result = $conn->query($sql);
 
-                /* $image = fopen('book-images/' . $pic, 'r');
-                    $Data = fread($image,filesize('$image'));
-                    fclose($image); */
-                echo "<div>";
+                    // Check if everything went alright
+                    if (!$result) {
+                        die("Something went wrong with SQL: " . $conn->error);
+                    } else {
+                        echo "<p>Found " . $result->num_rows . " uploaded books.</p>";
+                    }
+                    echo "</div>";
 
-                $sql = "SELECT * FROM collection";
-                $result = $conn->query($sql);
-
-                // Check if everything went alright
-                if (!$result) {
-                    die("Something went wrong with SQL: " . $conn->error);
-                } else {
-                    echo "<p>Found " . $result->num_rows . " uploaded books.</p>";
-                }
-                echo "</div>";
-
-                echo "<div class=\"book-card-wrapper\">";
-
-                while ($row = $result->fetch_assoc()) {
+                    echo "<div class=\"book-card-wrapper\">";
+                    
+                    while ($row = $result->fetch_assoc()) {
                     echo "<div class=\"book-card-holder\">";
                     echo "<div class=\"book-card\">";
-                    echo "<div><img src=\"bilder/tolkien.jpg\"></div>";
+                    echo "<div><img src=\"$row[image]\"></div>";
+                    
                     echo "<div class=\"book-title\">
                             <h3>$row[title]</h3>
                             <p>$row[author]</p>";
@@ -79,40 +76,32 @@ include "./conn.php";
                     echo "<button></button>";
                     echo "</div>";
                     echo "</div>";
-                }
-                echo "</div>";
-                ?>
-                <!-- <img src=\"bilder/tolkien.jpg\"> -->
+                    }
+                    echo "</div>";
+                    ?>
+            
             </div>
             <div class="request-book">
                 <div class="title">
                     <h2>Request book</h2>
                 </div>
                 <div class="request-wrapper">
-                    <form method="post" name="myemailform" action="book-request.php">
-                        <label for="name">Enter books title</label>
-                        <input type="text" name="name"> <br>
-
-                        <label for="author">Enter books author</label>
-                        <input type="text" name="author"> <br>
-
-                        <br> <input type="submit" value="Send">
-                    </form>
+                    <button class="genre">Thriller</button>
+                    <button class="genre">Horror</button>
+                    <button class="genre">Romance</button>
                 </div>
             </div>
         </div>
     </main>
     <script>
         function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
+          var x = document.getElementById("myTopnav");
+          if (x.className === "topnav") {
+            x.className += " responsive";
+          } else {
+            x.className = "topnav";
+          }
         }
-    </script>
-
+        </script>
 </body>
-
 </html>
